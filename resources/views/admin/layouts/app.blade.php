cb4391f333731fad27b1d9fe74ec2ce7cfdcbe5d
thuyen <20010928@st.phenikaa-uni.edu.vn>
2023-10-09 11:00:29 +0700

    creat file

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Laravel Shop :: Administrative Panel</title>
+		<!-- Google Font: Source Sans Pro -->
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+		<!-- Font Awesome -->
+		<link rel="stylesheet" href="{{ asset('admin-assets/plugins/fontawesome-free/css/all.min.css') }}">
+		<!-- Theme style -->
+		<link rel="stylesheet" href="{{ asset('admin-assets/css/adminlte.min.css') }}">
+		<link rel="stylesheet" href="{{ asset('admin-assets/css/custom.css') }}">
+	</head>
+	<body class="hold-transition sidebar-mini">
+		<!-- Site wrapper -->
+		<div class="wrapper">
+			<!-- Navbar -->
+			@include('admin.layouts.sidebar')
+			<!-- /.navbar -->
+			<!-- Main Sidebar Container -->
+			<aside class="main-sidebar sidebar-dark-primary elevation-4">
+				<!-- Brand Logo -->
+				<a href="#" class="brand-link">
+					<img src="{{ asset('admin-assets/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+					<span class="brand-text font-weight-light">QUẢN LÝ KHO</span>
+				</a>
+				<!-- Sidebar -->
+				<div class="sidebar">
+					<!-- Sidebar user (optional) -->
+					<nav class="mt-2">
+						<ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+							<!-- Add icons to the links using the .nav-icon class
+								with font-awesome or any other icon font library -->
+							<li class="nav-item">
+								<a href="dashboard.html" class="nav-link">
+									<i class="nav-icon fas fa-tachometer-alt"></i>
+									<p>Dashboard</p>
+								</a>																
+							</li>
+							<li class="nav-item">
+								<a href="categories.html" class="nav-link">
+									<i class="nav-icon fas fa-file-alt"></i>
+									<p>Category</p>
+								</a>
+							</li>
+							<li class="nav-item">
+								<a href="subcategory.html" class="nav-link">
+									<i class="nav-icon fas fa-file-alt"></i>
+									<p>Sub Category</p>
+								</a>
+							</li>
+							<li class="nav-item">
+								<a href="brands.html" class="nav-link">
+									<svg class="h-6 nav-icon w-6 shrink-0" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+										<path stroke-linecap="round" stroke-linejoin="round" d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
+									  </svg>
+									<p>Brands</p>
+								</a>
+							</li>
+							<li class="nav-item">
+								<a href="products.html" class="nav-link">
+									<i class="nav-icon fas fa-tag"></i>
+									<p>Products</p>
+								</a>
+							</li>
+							
+							<li class="nav-item">
+								<a href="#" class="nav-link">
+									<!-- <i class="nav-icon fas fa-tag"></i> -->
+									<i class="fas fa-truck nav-icon"></i>
+									<p>Shipping</p>
+								</a>
+							</li>							
+							<li class="nav-item">
+								<a href="orders.html" class="nav-link">
+									<i class="nav-icon fas fa-shopping-bag"></i>
+									<p>Orders</p>
+								</a>
+							</li>
+							<li class="nav-item">
+								<a href="discount.html" class="nav-link">
+									<i class="nav-icon  fa fa-percent" aria-hidden="true"></i>
+									<p>Discount</p>
+								</a>
+							</li>
+							<li class="nav-item">
+								<a href="users.html" class="nav-link">
+									<i class="nav-icon  fas fa-users"></i>
+									<p>Users</p>
+								</a>
+							</li>
+							<li class="nav-item">
+								<a href="pages.html" class="nav-link">
+									<i class="nav-icon  far fa-file-alt"></i>
+									<p>Pages</p>
+								</a>
+							</li>							
+						</ul>
+					</nav>
+					<!-- /.sidebar-menu -->
+				</div>
+				<!-- /.sidebar -->
+         	</aside>
+			<!-- Content Wrapper. Contains page content -->
+			<div class="content-wrapper">
+				@yield('content')
+			</div>
+			<!-- /.content-wrapper -->
+			<footer class="main-footer">
+				
+				<strong>Copyright &copy; 2014-2022 AmazingShop All rights reserved.
+			</footer>
+			
+		</div>
+		<!-- ./wrapper -->
+		<!-- jQuery -->
+		<script src="{{ asset('admin-assets/plugins/jquery/jquery.min.js') }}"></script>
+		<!-- Bootstrap 4 -->
+		<script src="{{ asset('admin-assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+		<!-- AdminLTE App -->
+		<script src="{{ asset('admin-assets/js/adminlte.min.js') }}"></script>
+		<!-- AdminLTE for demo purposes -->
+		<script src="{{ asset('admin-assets/js/demo.js') }}"></script>
+        @yield('customJs')
+	</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+use App\Http\Controllers\admin\AdminLoginController;
+use App\Http\Controllers\admin\HomeController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/admin/login', [AdminLoginController::class, 'index'])->name('admin.login');
+
+Route::group(['prefix' => 'admin'], function () {
+
+    Route::group(['middleware' => 'admin.guest'], function () {
+
+        Route::get('/login', [AdminLoginController::class, 'index'])->name('admin.login');
+        Route::post('/authenticate', [AdminLoginController::class, 'authenticate'])->name('admin.authenticate');
+    });
+
+    Route::group(['middleware' => 'admin.auth'], function () {
+        Route::get('/dashboard', [HomeController::class, 'index'])->name('admin.dashboard');
+        Route::get('/logout', [HomeController::class, 'logout'])->name('admin.logout');
+
+        //Category Routes
+        // Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
+        // Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
+
+        // Route::get('/getSlug', function (Request $request) {
+        //     $slug = '';
+        //     if (!empty($request->title)) {
+        //         $slug = Str::slug($request->title);
+        //     }
+
+        //     return response()->json([
+        //         'status' => true,
+        //         'slug' => $slug
+        //     ]);
+        // });
+    });
+});
